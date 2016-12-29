0d6b7877169965e0ca663c7c276a30df43ddd16f
Gabriel Lewis <benbowgl1@gmail.com>
2016-12-28 19:24:06 -0800

    complete first exercise

--- hello.js
+++ hello.js
@@ -1,1 +1,12 @@
-console.log('HELLO WORLD')
+console.log('starting app');
+
+const fs = require('fs');
+const os = require('os');
+
+fs.appendFile('greetings.txt', 'Hello world!' + os.username, (err) => {
+    if (err) {
+        console.log('Unable to write to file');
+    }
+});
+
+
