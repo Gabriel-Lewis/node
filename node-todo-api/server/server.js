108ac65a109749b3e3cc55e77e58b3c3fcb3a24b
Gabriel Lewis <benbowgl1@gmail.com>
2017-01-11 17:58:15 -0800

    node todo api hooked up to heroku

--- node-todo-api/server/server.js
+++ node-todo-api/server/server.js
@@ -1,5 +1,6 @@
 var express = require('express');
 var bodyParser = require('body-parser');
+const {ObjectID} = require('mongodb')
 
 var {mongoose} = require('./db/mongoose');
 var {Todo} = require('./models/todo');
@@ -9,6 +10,14 @@ var app = express()
 var port = process.env.PORT || 3000
 
 app.use(bodyParser.json())
+
+app.get('/todos', (req, res) => {
+  Todo.find().then((todos) => {
+    res.send({todos})
+  }, (e) => {
+    res.status(400).send(e)
+  })
+})
 
 app.post('/todos', (req, res) => {
   const todo = new Todo({
@@ -21,4 +30,23 @@ app.post('/todos', (req, res) => {
   })
 });
 
+app.get('/todos/:id', (req, res) => {
+  var id = req.params.id;
+
+  if (!ObjectID.isValid(id)) {
+    return res.status(404).send('404 page not found')
+  }
+
+  Todo.findById(id).then((todo) => {
+    if (!todo) {
+      return res.status(404).send('404 page not found')
+    }
+    res.send({todo});
+  }).catch((e) => res.send(e))
+});
+
 app.listen(port)
+
+module.exports = {
+  app
+}
